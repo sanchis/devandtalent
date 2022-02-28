51dab2970aab084c49d811d39b24952ef0526de3
Adrian Sanchis Gallego <adrian.sanchis@jobandtalent.com>
2022-02-28 15:16:59 +0100

    refactor for hexagonal architecture

--- src/infra/interfaces/http/index.ts
+++ src/infra/interfaces/http/index.ts
@@ -1,14 +1,23 @@
-import Fastify from 'fastify'
+import Fastify, { FastifyError, FastifyRequest } from 'fastify'
 import pluginsRegister from './plugin-register'
 import 'dotenv/config'
 import LoggerConfig from './utils/LoggerConfig'
 import { createConnection } from 'typeorm'
+import Rollbar from 'rollbar'
 
 const app = Fastify({
   logger: LoggerConfig()
 })
+const rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
 
+  captureUnhandledRejections: true
+})
 void app.register(pluginsRegister)
+app.setErrorHandler((error: FastifyError, request: FastifyRequest) => {
+  rollbar.log(error)
+})
 
 app.listen(3000, '0.0.0.0', (error: Error | null, address: string) => {
   if (error !== null) {
--- src/infra/interfaces/http/routes/cola/hola.ts
+++ src/infra/interfaces/http/routes/cola/hola.ts
@@ -2,6 +2,7 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 
 export default async function (fastify: FastifyInstance): Promise<void> {
   fastify.get('/hola', async (request: FastifyRequest, reply: FastifyReply) => {
+    reply.badGateway()
     void reply.send({ result: 'd' })
   })
 }
