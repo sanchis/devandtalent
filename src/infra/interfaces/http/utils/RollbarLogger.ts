d227d5d49f6f1b89b01c68f646b5a875f872fc41
Adrian Sanchis Gallego <adrian.sanchis@jobandtalent.com>
2022-03-09 12:52:22 +0100

    refactor rollbar init

--- src/infra/interfaces/http/utils/RollbarLogger.ts
+++ src/infra/interfaces/http/utils/RollbarLogger.ts
@@ -1,11 +1,6 @@
 import { FastifyError, FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
-import Rollbar from 'rollbar'
+import rollbar from '../../rollbar'
 
-const rollbar = new Rollbar({
-  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
-  captureUncaught: true,
-  captureUnhandledRejections: true
-})
 export default async function (fastify: FastifyInstance): Promise<void> {
   void fastify.setErrorHandler((error: FastifyError, request: FastifyRequest, reply: FastifyReply) => {
     rollbar.error(error)
---
+++ src/infra/interfaces/rollbar.ts
@@ -0,0 +1,9 @@
+import Rollbar from 'rollbar'
+
+const rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+})
+
+export default rollbar
